441975eaa2491db333aac362083d53dc012ccd37
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-05-01 14:48:33 +0200

    articles page w/functional load more button

---
+++ js/listOfPosts.js
@@ -0,0 +1,74 @@
+const content = document.querySelector(".article-box");
+const loadMoreBtn = document.querySelector(".cta");
+
+//hide the button while the api is being called
+loadMoreBtn.style.display = "none";
+
+//call api
+let url =
+  "https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?acf_format=standard";
+
+async function fetchPosts() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    createHTML(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//call api
+loadMoreBtn.addEventListener("click", () => {
+  url =
+    "https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?per_page=100&acf_format=standard";
+  fetchPosts();
+});
+
+fetchPosts();
+
+function createHTML(data) {
+  for (let i = 0; i < data.length; i++) {
+    if (i === 0) {
+      content.innerHTML = `  <div class="ft-post-box">
+                                    <div>
+                                    <img
+                                        class="ft-post-img"
+                                        src="${data[0].acf.featured_img}"
+                                    />
+                                    </div>
+                                    <div>
+                                    <div>
+                                        <span class="tags">#${data[0].acf.tag_1}</span>
+                                        <span class="tags">#${data[0].acf.tag_2}</span>
+                                    </div>
+                                    <h2>${data[0].title.rendered}</h2>
+                                    <p>
+                                        ${data[0].acf.brief}
+                                    </p>
+                                    </div>
+                                </div>`;
+    } else if (i < data.length) {
+      content.innerHTML += `  <div class="post-card">
+                                    <div>
+                                        <img
+                                        class="post-img"
+                                        src="${data[i].acf.featured_img}"
+                                        />
+                                    </div>
+                                    <div>
+                                        <div>
+                                        <span class="tags">#${data[i].acf.tag_1}</span>
+                                        <span class="tags">#${data[i].acf.tag_2}</span>
+                                        </div>
+                                        <h2>${data[i].title.rendered}</h2>
+                                        <p>
+                                        ${data[i].acf.brief}
+                                    </p>
+                                    </div>
+                                    </div>`;
+    }
+  }
+  //display button
+  loadMoreBtn.style.display = "block";
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
+//select elements in the dom
+const slider = document.querySelector(".slider-cards-box");
+const btnRight = document.querySelector(".right-arrow");
+const btnLeft = document.querySelector(".left-arrow");
 
+//call api
+const url =
+  "https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?per_page=20&acf_format=standard";
+
+async function fetchPosts() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    createHTML(data);
+
+    btnRight.addEventListener("click", () => {
+      slider.innerHTML = "";
+      createHTML(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchPosts();
+
+// let postNumber = 0;
+
+function createHTML(data) {
+  for (let i = 0; i < data.length; i++) {
+    // console.log(data[postNumber].id);
+    slider.innerHTML += `
+      <div class="slider-card">
+        <div>
+          <img class="slider-img" src="${data[i].acf.featured_img}" />
+        </div>
+        <span class="tags">#${data[i].acf.tag_1}</span>
+        <span class="tags">#${data[i].acf.tag_2}</span>
+        <h3>${data[i].title.rendered}</h3>
+        <p class="slider-paragraph">
+        ${data[i].acf.brief}
+        </p>
+      </div>
+    `;
+
+    // postNumber++;
+    //get browser size and set variables for different screen sizes
+    const browserSize = window.outerWidth;
+    //render number of cards according to screen size
+
+    if (browserSize <= 599 && i === 0) {
+      break;
+    } else if (browserSize > 599 && browserSize <= 999 && i === 1) {
+      break;
+    } else if (browserSize > 999 && browserSize <= 1199 && i === 2) {
+      break;
+    } else if (browserSize > 1200 && i === 3) {
+      break;
+    }
+  }
+}
