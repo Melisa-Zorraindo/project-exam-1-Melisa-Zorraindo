8f3b97f10cac0a9bbed942d8e643af0057271e0b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-05-02 11:32:06 +0200

    modal for specific article

--- js/specificArticle.js
+++ js/specificArticle.js
@@ -5,13 +5,14 @@ const param = new URLSearchParams(queryString);
 const id = param.get("id");
 const url = `https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?include=${id}&acf_format=standard`;
 
-console.log(url);
+//for the popup
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 async function getArticle() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data[0]);
     renderHTML(data[0]);
   } catch (error) {
     console.log(error);
@@ -23,6 +24,8 @@ getArticle();
 function renderHTML(data) {
   //update title
   newTitle.innerHTML = `FitFactory | ${data.title.rendered}`;
+
+  //render HTML
   articleContainer.innerHTML = `<article>
                                     <h1>${data.acf.subtitle}</h1>
                                     <h2>${data.title.rendered}</h2>
@@ -47,4 +50,18 @@ function renderHTML(data) {
                                         <button class="cta">submit</button>
                                     </div>
                                 </article>`;
+
+  //open and close modal popup when img clicked
+  const img = document.querySelector(".featured-img");
+  img.addEventListener("click", () => {
+    img.classList.add("modal-img");
+    body.style.overflow = "hidden";
+    overlay.classList.remove("hidden");
+  });
+
+  overlay.addEventListener("click", () => {
+    img.classList.remove("modal-img");
+    overlay.classList.add("hidden");
+    body.style.overflow = "auto";
+  });
 }
