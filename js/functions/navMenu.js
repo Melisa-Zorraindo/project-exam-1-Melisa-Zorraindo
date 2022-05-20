6c5a45df17a7e4a229be9d74eb7e72298702e30b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-05-20 11:17:50 +0200

    fix bug hamburger menu

--- js/functions/navMenu.js
+++ js/functions/navMenu.js
@@ -5,13 +5,12 @@ const navBar = document.querySelector("nav");
 navMenuBtn.addEventListener("click", toggleNavMenu);
 
 function toggleNavMenu() {
-  if (navBar.style.transform === "translateY(-200%)") {
-    console.log(navBar);
-    navBar.style.transform = "translateY(0)";
+  if (navBar.classList.contains("nav-mobile")) {
+    navBar.classList.remove("nav-mobile");
     menuIcon.classList.remove("fa-bars");
     menuIcon.classList.add("fa-times");
   } else {
-    navBar.style.transform = "translateY(-200%)";
+    navBar.classList.add("nav-mobile");
     menuIcon.classList.remove("fa-times");
     menuIcon.classList.add("fa-bars");
   }
