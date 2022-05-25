cb444f10e51f3becf1c2a6a14a50d14bb1a8e482
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-05-25 15:21:29 +0200

    fix media queries hamburger menu

--- js/functions/navMenu.js
+++ js/functions/navMenu.js
@@ -1,22 +1,9 @@
 const navMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
-// const menuIcon = document.querySelector(".open-menu i");
 const navBar = document.querySelector("nav");
 
 navMenuBtn.addEventListener("click", openMenu);
 closeMenuBtn.addEventListener("click", closeMenu);
-
-/* function toggleNavMenu() {
-  if (navBar.classList.contains("nav-mobile")) {
-    navBar.classList.remove("nav-mobile");
-    menuIcon.classList.remove("fa-bars");
-    menuIcon.classList.add("fa-times");
-  } else {
-    navBar.classList.add("nav-mobile");
-    menuIcon.classList.remove("fa-times");
-    menuIcon.classList.add("fa-bars");
-  }
-} */
 
 function openMenu() {
   navBar.style.transform = "translateY(0)";
