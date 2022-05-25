70c40a6a3872579429996ce4301a993f06efc173
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-05-25 10:47:11 +0200

    responsive carousel part I

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 //select elements in the dom
-const prev = document.querySelector(".prev");
+const slider = document.querySelector(".slider");
+document.addEventListener("click", (e) => {
+  let handle;
+  if (e.target.matches(".handle")) {
+    handle = e.target;
+  } else {
+    handle = e.target.closest(".handle");
+  }
+
+  if (handle != null) {
+    onHandleClick(handle);
+  }
+});
+
+function onHandleClick(handle) {
+  const slider = handle.closest(".container").querySelector(".slider");
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+  );
+  if (handle.classList.contains("left-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex - 1);
+  } else if (handle.classList.contains("right-handle")) {
+    slider.style.setProperty("--slider-index", sliderIndex + 1);
+  }
+}
+/* const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const track = document.querySelector(".track");
-let carouselWidth = document.querySelector(".slider-box").offsetWidth;
+let carouselWidth = document.querySelector(".slider-box").offsetWidth; */
 
 //get browser size and set variables for different screen sizes
-const browserSize = window.outerWidth;
+// const browserSize = window.outerWidth;
 //render number of cards according to screen size
-let itemsPerPage;
+/* let itemsPerPage;
 if (browserSize <= 699) {
   itemsPerPage = 3;
 } else if (browserSize >= 700 && browserSize <= 1311) {
@@ -20,8 +45,8 @@ if (browserSize <= 699) {
   next.style.display = "block";
   prev.style.display = "block";
   prev.disabled = true;
-}
-let url = `https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?per_page=${itemsPerPage}&acf_format=standard`;
+} */
+let url = `https://fitfactory.melisazor.com/wordpress/wp-json/wp/v2/articles?per_page=12&acf_format=standard`;
 
 //call api
 async function fetchPosts() {
@@ -39,26 +64,23 @@ fetchPosts();
 
 //render HTML
 function createHTML(data) {
-  track.innerHTML = "";
+  slider.innerHTML = "";
   data.forEach((post) => {
-    track.innerHTML += `
-    <div class="slider-card-box">
-                <a class="slider-card" href="../pages/article.html?id=${post.id}">
-                <h3 class="slider-card-info">
-                    ${post.title.rendered}
-                  </h3>
-                  <div class="slider-card-img" style="background-image:url('${post.acf.featured_img.url}')" alt="${post.acf.featured_img.alt}"></div>
-                  <div>
+    slider.innerHTML += `
+              <a class="slider-card" href="../pages/article.html?id=${post.id}">
+                <h3>${post.title.rendered}</h3>
+                <img src="${post.acf.featured_img.url}" alt="${post.acf.featured_img.alt}" />
+                <div>
                     <span class="tags">#${post.acf.tag_1}</span>
                     <span class="tags">#${post.acf.tag_2}</span>
-                  </div>
-                </a>
-              </div>`;
+                </div>
+              </a>
+            `;
   });
 }
 
 //functionality for slider desktop
-let index = 0;
+/* let index = 0;
 
 next.addEventListener("click", () => {
   index++;
@@ -78,7 +100,7 @@ prev.addEventListener("click", () => {
   if (index === 0) {
     prev.disabled = true;
   }
-});
+}); */
 
 /* if (track.offsetWidth - index * carouselWidth < carouselWidth){
     next.disabled = true;
